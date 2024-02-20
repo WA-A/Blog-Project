e9d46130ca4d72cc959dd1356c74f6a7ee83cffb
wasan <wasanawwad2020@gmail.com>
2024-02-20 19:29:03 +0200

    Update User

--- src/module/User/User.Controller.js
+++ src/module/User/User.Controller.js
@@ -1,1 +1,18 @@
 import UserModel from "../../../DB/moudle/UserModule.js";
+export const Update = async (req,res)=>{
+    try{
+    const {id} = req.params;
+    const {name} = req.body;
+
+    const user = await UserModel.update({name},{where:{id}});
+
+        if(!user){
+            return res.json({message:"user not found"});
+        }
+    
+        return res.json({message:"success",users:user});
+    }
+    catch(error){
+        return res.json({message:"error",error:error.stack});
+    } 
+ }
--- src/module/User/User.Router.js
+++ src/module/User/User.Router.js
@@ -2,5 +2,5 @@ import {Router} from 'express';
 import * as UserController from '../User/User.Controller.js';
 const router = Router();
 
-
+router.put('/update/:id',UserController.Update);
 export default router ;
