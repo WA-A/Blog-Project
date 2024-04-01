8498532dd44131789995cc239360ca36ee100551
wasan <wasanawwad2020@gmail.com>
2024-04-01 03:21:49 +0300

    Edit Front Code

--- Front-Blog/main.js
+++ Front-Blog/main.js
@@ -1,6 +1,20 @@
 const getUsers = async() => {
     const {data} = await axios.get(`https://blog-project-2-02xi.onrender.com/user/getuser`);
      console.log(data);
+     return data.users;
 };
 
-getUsers();
+const displayUsers = async()=>{
+    const users = await getUsers();
+    const result = users.map( user=>
+        ` <div class='col-lg-4'>
+        <h2>${user.name}</h2>
+        </div>
+        `
+    ).join('');
+    document.querySelector(".users .row").innerHTML=result;
+    }
+
+    displayUsers();
+
+   
